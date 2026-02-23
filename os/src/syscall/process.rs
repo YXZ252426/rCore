244499d576e8681ca4f27250302b40ae36ad3510
yxz <3069946891@qq.com>
2026-02-23 10:36:18 +0800

    impl sys_trace

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -1,5 +1,5 @@
 //! Process management syscalls
-use crate::task::{change_program_brk, exit_current_and_run_next, suspend_current_and_run_next};
+use crate::{mm::translated_byte_buffer, task::{change_program_brk, current_user_token, exit_current_and_run_next, suspend_current_and_run_next}, timer::get_time_us};
 
 #[repr(C)]
 #[derive(Debug)]
@@ -25,16 +25,39 @@ pub fn sys_yield() -> isize {
 /// YOUR JOB: get time with second and microsecond
 /// HINT: You might reimplement it with virtual memory management.
 /// HINT: What if [`TimeVal`] is splitted by two pages ?
-pub fn sys_get_time(_ts: *mut TimeVal, _tz: usize) -> isize {
-    trace!("kernel: sys_get_time");
-    -1
+pub fn sys_get_time(ts: *mut TimeVal, tz: usize) -> isize {
+    let token = current_user_token();
+    let size = core::mem::size_of::<TimeVal>();
+
+    let mut buffers = translated_byte_buffer(token, ts as *const u8, tz);
+    let us = get_time_us();
+    let time_val = TimeVal {
+        sec: us / 1_000_000,
+        usec: us % 1_000_000
+    };
+    let time_val_bytes = unsafe {
+        core::slice::from_raw_parts(
+            &time_val as *const _ as *const u8,
+            size
+        )
+    };
+
+    let mut bytes_written = 0;
+    for buffer in buffers.iter_mut() {
+        let len = buffer.len().min(time_val_bytes.len() - bytes_written);
+        if len == 0 {
+            break;
+        }
+        buffer[..len].copy_from_slice(&time_val_bytes[bytes_written..bytes_written + len]);
+        bytes_written += len;
+    }
+    0
 }
 
 /// TODO: Finish sys_trace to pass testcases
 /// HINT: You might reimplement it with virtual memory management.
 pub fn sys_trace(_trace_request: usize, _id: usize, _data: usize) -> isize {
-    trace!("kernel: sys_trace");
-    -1
+
 }
 
 // YOUR JOB: Implement mmap.
